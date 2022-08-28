761c4484fd0c8c76ef0c6b63852864118369f472
Hxy <huxiaoyang1208@qq.com>
2022-08-28 15:32:19 +0800

    v.0.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
+int n, c, a[200005];
+multiset<int> mp;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    scanf("%d%d", &n, &c);
+    for (int i(1); i <= n; ++i) {
+        scanf("%d", &a[i]);
+        mp.insert(a[i]);
+    }
+    int ans = 0;
+    for (int i(1); i <= n; ++i) {
+        ans += mp.count(a[i] - c);
+    }
+    cout << ans;
     return 0;
 }
