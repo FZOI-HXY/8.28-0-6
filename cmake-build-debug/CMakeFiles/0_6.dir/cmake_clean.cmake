df8e6d539e195b691d98aaa15f0bedc5f47180c9
Hxy <huxiaoyang1208@qq.com>
2022-08-28 15:19:55 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/0_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "0_6.exe"
+  "0_6.exe.manifest"
+  "0_6.pdb"
+  "CMakeFiles/0_6.dir/main.cpp.obj"
+  "lib0_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
